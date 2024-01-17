6299fa6b868327364710ba5eb58cf22f656f58e7
Pasha Kucherov <dev.kucherov@gmail.com>
2024-01-17 21:02:37 +0300

    сверстать блок contact

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,26 +1,19 @@
-// import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 
-// import styles bundle
 import 'swiper/css/bundle';
 
-// init Swiper:
 export const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   slidesPerView: 1,
   centeredSlides: true,
-  spaceBetween: 50,
+  spaceBetween: 20,
 
-  // freeMode: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '#sliderNext',
     prevEl: '#sliderPrev',
@@ -30,19 +23,10 @@ export const swiper = new Swiper('.swiper', {
   },
 
   breakpoints: {
-    // 600: {
-    //   slidesPerView: 1,
-    //   centeredSlides: true,
-    //   spaceBetween: 50,
-    // },
     841: {
       slidesPerView: 2,
       centeredSlides: true,
     },
-    // 1230: {
-    //   slidesPerView: 2,
-    //   centeredSlides: true,
-    // },
   },
 });
 
